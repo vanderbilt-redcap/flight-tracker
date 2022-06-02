bd4d1e51df889a2665b966049e6963e9f3de7b09
Scott J. Pearson <scott.j.pearson@vumc.org>
2022-06-02 18:06:14 -0500

    4.11.2

--- CareerDev.php
+++ CareerDev.php
@@ -15,7 +15,7 @@ class CareerDev {
 	public static $passedModule = NULL;
 
 	public static function getVersion() {
-		return "4.11.1";
+		return "4.11.2";
 	}
 
 	public static function getLockFile($pid) {
--- clean/resetLogs.php
+++ clean/resetLogs.php
@@ -1,22 +1,27 @@
 <?php
 
-$pid = $_GET['pid'];
-try {
-	if (!$pid) {
-		die("You must specify a pid!");
+require_once(APP_PATH_DOCROOT."Classes/System.php");
 
-        	$fromAndWhereClause = "FROM redcap_external_modules_log AS l INNER JOIN redcap_external_modules AS m ON m.external_module_id = l.external_module_id WHERE m.directory_prefix = 'flight_tracker' AND l.project_id = ?";
-		$iteration = 0;
-        	do {
-			$iteration++;
-            		$deleteSql = "DELETE $fromAndWhereClause ORDER BY l.log_id LIMIT 10000";
-            		$selectSql = "SELECT l.log_id $fromAndWhereClause ORDER BY l.log_id LIMIT 1";
-            		$this->query($deleteSql, [$pid]);
-            		$result = $this->query($selectSql, [$pid]);
-            		$moreToDelete = $result && $result->fetch_assoc();
-        	} while ($moreToDelete && ($iteration < 50000));
-		echo "Success.";
-	}
+$hours = 16;
+\System::increaseMaxExecTime($hours * 3600);
+
+$pid = $_GET['pid'];
+if (!$pid) {
+    die("You must specify a pid!");
+}
+
+try {
+    $fromAndWhereClause = "FROM redcap_external_modules_log AS l INNER JOIN redcap_external_modules AS m ON m.external_module_id = l.external_module_id WHERE m.directory_prefix = 'flight_tracker' AND l.project_id = ?";
+    $iteration = 0;
+    do {
+        $iteration++;
+        $deleteSql = "DELETE $fromAndWhereClause ORDER BY l.log_id LIMIT 10000";
+        $selectSql = "SELECT l.log_id $fromAndWhereClause ORDER BY l.log_id LIMIT 1";
+        $this->query($deleteSql, [$pid]);
+        $result = $this->query($selectSql, [$pid]);
+        $moreToDelete = $result && $result->fetch_assoc();
+    } while ($moreToDelete && ($iteration < 50000));
+    echo "Success.";
 } catch(\Exception $e) {
 	echo "ERROR: ".$e->getMessage();
 }
