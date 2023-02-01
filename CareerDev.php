245e546cebc5cd54def156a4980ec87220015cbe
Scott J. Pearson <scott.j.pearson@vumc.org>
2023-02-01 15:46:14 -0600

    5.0.3

--- CareerDev.php
+++ CareerDev.php
@@ -16,7 +16,7 @@ class CareerDev {
 	public static $passedModule = NULL;
 
 	public static function getVersion() {
-		return "5.0.2";
+		return "5.0.3";
 	}
 
 	public static function getLockFile($pid) {
