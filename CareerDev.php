f4e0060ec028d51c2ee405fd54235177933e64e5
Scott J. Pearson <scott.j.pearson@vumc.org>
2023-06-29 17:38:38 -0500

    5.10.3

--- CareerDev.php
+++ CareerDev.php
@@ -22,7 +22,7 @@ class CareerDev {
 	public static $passedModule = NULL;
 
 	public static function getVersion() {
-		return "5.10.2";
+		return "5.10.3";
 	}
 
 	public static function getLockFile($pid) {
--- classes/EmailManager.php
+++ classes/EmailManager.php
@@ -136,13 +136,17 @@ class EmailManager {
 	# $to, if specified, denotes a test email
 	# main way of sending emails
 	public function sendRelevantEmails($to = "", $names = array()) {
+	$this->turnOffShutdownFunction = FALSE;
         register_shutdown_function([$this, "reportCronErrors"]);
         $messages = $this->enqueueRelevantEmails($to, $names, "sendEmail");
 		$this->sendPreparedEmails($messages, ($to !== ""));
+	$this->turnOffShutdownFunction = TRUE;
 	}
 
     public static function reportCronErrors() {
-        CronManager::reportCronErrors("email");
+	if (!$this->turnOffShutdownFunction) {
+        	CronManager::reportCronErrors("email");
+	}
     }
 
 	private static function transformToTS($datetime) {
@@ -1593,4 +1597,5 @@ a.button { font-weight: bold; background-image: linear-gradient(45deg, #fff, #dd
     private $adminEmail;
     private $defaultFrom;
     private $preparingMins;
+	private $turnOffShutdownFunction = TRUE;
 }
