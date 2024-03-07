35aadf9e4e9b1e81470867b2ed0dec5e220459ae
Scott J. Pearson <scott.j.pearson@vumc.org>
2024-03-07 11:31:51 -0600

    6.9.0b

--- dashboard/activities.php
+++ dashboard/activities.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Vanderbilt\FlightTrackerExternalModule;
+
 use \Vanderbilt\CareerDevLibrary\Download;
 use \Vanderbilt\CareerDevLibrary\Application;
 use \Vanderbilt\CareerDevLibrary\Measurement;
