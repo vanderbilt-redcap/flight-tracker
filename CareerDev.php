c3809e6997f00d5610148e9467eadd27286d5724
Scott J. Pearson <scott.j.pearson@vumc.org>
2024-11-21 17:36:02 -0600

    6.18.1

--- CareerDev.php
+++ CareerDev.php
@@ -17,7 +17,7 @@ class CareerDev {
 	public static $passedModule = NULL;
 
 	public static function getVersion() {
-		return "6.18.0";
+		return "6.18.1";
 	}
 
     public static function getLocalhostPluginPid() {
